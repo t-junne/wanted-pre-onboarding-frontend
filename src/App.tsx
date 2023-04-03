8a13437c3c5beab8cfd0695534599bc1eb2193a8
t-junne <tjunne@gmail.com>
2023-04-04 07:03:43 +0900

    리다이렉션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import { TodoPage } from './pages/todo'
 import { RequireAuth } from './components/auth/requireAuth'
 
 function App() {
+  const token = window.localStorage.getItem('access_token')
+  
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<Navigate to='/todo' />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/signin' element={<SignInPage />} />
+        <Route path='/' element={<Navigate to='todo' />} />
+        <Route path='signup' element={token ? <TodoPage /> : <SignUpPage />} />
+        <Route path='signin' element={token ? <TodoPage /> : <SignInPage />} />
         <Route element={<RequireAuth />}>
-          <Route path='/todo' element={<TodoPage />} />
+          <Route path='todo' element={<TodoPage />} />
         </Route>
       </Routes>
     </div>
