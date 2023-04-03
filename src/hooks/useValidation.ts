49ab02b7cfc154c3eedb52ef85e7009d88694927
t-junne <tjunne@gmail.com>
2023-04-04 08:49:28 +0900

    Media 추가

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -1,18 +1,20 @@
+import { useCallback } from "react"
+
 export const useValidation = () => {
-  const validateEmail = (email: string) => {
+  const validateEmail = useCallback((email: string) => {
     const emailRegEx = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([-_.]?[0-9a-zA-Z])/i
     if (email.length >= 1 && !emailRegEx.test(email)) {
       return false
     }
     return true
-  }
+  }, [])
 
-  const validatePassword = (password: string) => {
+  const validatePassword = useCallback((password: string) => {
     if (password.length >= 1 && password.length < 8) {
       return false
     }
     return true
-  }
+  }, [])
 
   return {
     validateEmail,
